b9e61d3e9c645f805768acdf030549aa617c402f
Anushtha <kumarsen.bash@gmail.com>
2024-10-19 19:32:43 +0530

    server api

--- frontend/src/constants/globals.js
+++ frontend/src/constants/globals.js
@@ -1,5 +1,6 @@
 const globals = {
-	API_BASE_URL: "http://localhost:5000/api/",
+	// API_BASE_URL: "http://localhost:5000/api/",
+	API_BASE_URL: "https://cuvette-assesment.onrender.com/api/",
 };
 
 export default globals;
